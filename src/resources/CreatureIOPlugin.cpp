0e2de6e0f9348ddcd130e1ec077838339a8d0de9
Lirrec <git@lirrec.de>
2012-12-17 17:39:05 +0100

    - rewrote CreatureIOPlugin to iterate the ptree instead of assigning numbers to the stored objects

--- src/resources/CreatureIOPlugin.cpp
+++ src/resources/CreatureIOPlugin.cpp
@@ -3,51 +3,53 @@
 
 using boost::property_tree::ptree;
 
-CreatureIOPlugin::ObjectList CreatureIOPlugin::loadObjects(ptree &r){
-	int i = r.get<int>("creatures.count");
-	stringstream ss;
-	ObjectList ol;
+CreatureIOPlugin::ObjPtr CreatureIOPlugin::loadObject(const boost::property_tree::ptree::value_type &node)
+{
+	ObjPtr re;
+	if (node.first != "Creature") return re;
 
-	while(i){
-		// better use get_child here instead of a counter,
-		// see: http://www.boost.org/doc/libs/1_52_0/doc/html/boost/property_tree/basic_ptree.html#id1249113-bb
-		// and: http://www.boost.org/doc/libs/1_52_0/doc/html/boost_propertytree/tutorial.html
-		auto c = std::shared_ptr<Creature>(new Creature());
-		c->setCurrentHealth(get<int>(r, i, "currentHealth"));
-		c->setAge(get<int>(r, i, "age"));
-		c->setPosition(get<float>(r, i, "posX"), get<float>(r, i, "posY"));
+	try
+	{
+		const ptree& pt = node.second;
+
+		re.reset( new Creature() );
+
+		re->setCurrentHealth( pt.get<int>("currentHealth") );
+		re->setAge			( pt.get<int>("age") );
+		re->setPosition		( pt.get<float>("pos.y"), pt.get<float>(r, i, "pos.y") );
 		//c->setSpecies(get<string>(r, i, "species")); // how to save? or restore?
 		//c->setCurrentTile(get<well, ...>(r, i, "tile")); // or is position enough?
-		ol.push_back(c);
-		--i;
+
 	}
-	return ol;
+	catch ( boost::property_tree::ptree_error )
+	{
+		Engine::out() << "[CreatureIOPlugin] Error loading Creature from ptree!"
+		return re.reset();
+	}
+
+	return re;
 }
 
-template<typename T>
-T CreatureIOPlugin::get(ptree &r, const int cnum, const string &key){
-	stringstream ss;
-	ss << "creatures." << cnum << "." << key;
-	return r.get<T>(ss.str());
-}
 
-template<typename T>
-void CreatureIOPlugin::put(ptree &r, const int cnum, const string &key, const T &value){
-	stringstream ss;
-	ss << "creatures." << cnum << "." << key;
-	r.put<T>(ss.str(), value);
-}
+bool CreatureIOPlugin::saveObject(const Creature &c, boost::property_tree::ptree &r)
+{
+	try
+	{
+		ptree pt;
 
-bool CreatureIOPlugin::saveObject(const Creature &c, boost::property_tree::ptree &r){
-	int i = r.get<int>("creatures.count") +1;
+		pt.put<int>("currentHealth", c.getCurrentHealth());
+		pt.put<int>(r, i, "age", c.getAge());
+		pt.put<float>(r, i, "pos.x", c.getPosition().x());
+		pt.put<float>(r, i, "pos.y", c.getPosition().y());
+		//save species
+		//save tile?
 
-	put<int>(r, i, "currentHealth", c.getCurrentHealth());
-	put<int>(r, i, "age", c.getAge());
-	put<float>(r, i, "posX", c.getPosition().x());
-	put<float>(r, i, "posY", c.getPosition().y());
-	//save species
-	//save tile?
-
-	r.put<int>("creatures.count", i);
-	return true;
+		r.add( "Creature", pt);
+		return true;
+	}
+	catch ( boost::property_tree::ptree_error )
+	{
+		Engine::out() << "[CreatureIOPlugin] Error saving Creature to ptree!"
+		return false;
+	}
 }
--- src/resources/CreatureIOPlugin.hpp
+++ src/resources/CreatureIOPlugin.hpp
@@ -9,26 +9,18 @@
 #include "sbe/io/IOPlugin.hpp"
 #include "../simulator/Creature.hpp"
 
-using namespace std;
 
 class CreatureIOPlugin : public iTreeIOPlugin<Creature> {
-	CreatureIOPlugin(){}
+
+	CreatureIOPlugin()
+	{
+		file = "Creatures.info";
+	}
 	virtual ~CreatureIOPlugin(){}
 
-#ifndef __GCC_4_6__ // gcc < 4.7 doesn't support override
-	ObjectList loadObjects(boost::property_tree::ptree &root) override;
-	bool saveObject( const Creature &o, boost::property_tree::ptree &root) override;
-#else
-	ObjectList loadObjects(boost::property_tree::ptree &root);
-	bool saveObject( const Creature &o, boost::property_tree::ptree &root);
-#endif
 
-template<typename T>
-T get(boost::property_tree::ptree &r, const int cnum, const string &key);
-template<typename T>
-void put(boost::property_tree::ptree &r, const int cnum, const string &key, const T &value);
-
-
+	virtual ObjPtr loadObject(const boost::property_tree::ptree::value_type &node);
+	virtual bool saveObject( const Creature &o, boost::property_tree::ptree &root);
 
 };
 
