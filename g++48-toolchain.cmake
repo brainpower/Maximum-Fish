c1f48584d38ada29127fc292ef25168273e01845
brainpower <brainpower@gulli.com>
2013-01-07 14:53:59 +0100

    * added some gitignores, added cmake toolchain file for g++4.8

---
+++ g++48-toolchain.cmake
@@ -0,0 +1,10 @@
+
+#set(CMAKE_FIND_ROOT_PATH /usr)
+set(CMAKE_C_COMPILER "gcc-4.8" )
+set(CMAKE_CXX_COMPILER "g++-4.8" )
+#set(CMAKE_AR "gcc-ar-4.8" )
+#set(CMAKE_RANLIB "gcc-ranlib-4.8" )
+
+## if you want to use libc++ on a gnu system:
+#set(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++" CACHE STRING "")
+#add_definitions("-stdlib=libc++")
