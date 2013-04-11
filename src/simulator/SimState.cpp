2655c3af34d3c8aaf7c1820774a106f26a36789f
brainpower <brainpower@gulli.com>
2013-04-11 19:33:22 +0200

    * fixing sim state

--- src/simulator/SimState.cpp
+++ src/simulator/SimState.cpp
@@ -1,19 +1,22 @@
 #include "SimState.hpp"
 
+#include "world/Terrain.hpp"
+#include "world/Creature.hpp"
+
 SimState::SimState(const SimState &o)
 	: _currentTick(o._currentTick),
-	  _currentSeed(_currentSeed),
+	  _currentSeed(o._currentSeed),
 	  _numGenerated(o._numGenerated),
 	  _terrain(o._terrain),
 	  _species(o._species){
 
-	auto tl = _terrain.getTileList();
+	auto tl = _terrain->getTileList();
 	for(auto it=tl.begin(); it != tl.end(); ++it) // clear creatures list of Tiles, working?
-		it->getCreatures()->clear();
+		(*it)->getCreatures().clear();
 
 	for(auto it=o._creatures.begin(); it != o._creatures.end(); ++it){ // deep copy of creatures
 		_creatures.push_back(std::shared_ptr<Creature>(new Creature(**it)));
-		_creatures.back().updateTileFromPos(_terrain);
+		_creatures.back()->updateTileFromPos(_terrain);
 	}
 
 }
