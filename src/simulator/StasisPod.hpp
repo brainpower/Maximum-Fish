3cdbb1be91bef64204f144a0f6c0c657b85ec8d3
brainpower <brainpower@gulli.com>
2013-05-14 12:02:22 +0200

    * added peek and taw for getting a state that was frozen before given tick

--- src/simulator/StasisPod.hpp
+++ src/simulator/StasisPod.hpp
@@ -14,8 +14,10 @@ public:
 	void freeze(std::shared_ptr<SimState> s);
 	std::shared_ptr<SimState> taw(const size_t i);
 	std::shared_ptr<SimState> tawTop();
+	std::shared_ptr<SimState> tawTick(const int i);
 	const std::shared_ptr<SimState> peek(const size_t i);
 	const std::shared_ptr<SimState> peekTop();
+	const std::shared_ptr<SimState> peekTick(const int i);
 
 private:
 	friend class StasisPodIOPlugin;
